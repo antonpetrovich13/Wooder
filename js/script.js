6b40632a6d9c3d6d545c7addbc7b86bffcce1155
Anton Petrovich <63902821+antonpetrovich13@users.noreply.github.com>
2021-05-03 22:29:58 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -98,54 +98,19 @@ if (menuLinks.length > 0) {
 	}
 }
 */
-/*
+
 //СЛАЙДЕР MAIN
 let swiper = new Swiper('.main__slider', {
-	speed: 800,
-	parallax: true,
-	navigation: {
-		nextEl: '.main__next',
-		prevEl: '.main__prev',
-	},
-});
-
-let lots = new Swiper('.lots__slider', {
 	speed: 500,
-	keyboard: true,
-	loop: true,
-	effect: 'coverflow',
-	navigation: {
-		nextEl: '.navigation__next',
-		prevEl: '.navigation__prev',
-	},
-	breakpoints: {
-		280: {
-			slidesPerView: 1,
-			effect: 'slide',
-			autoHeight: true,
-		},
-		600: {
-			slidesPerView: 2,
-			effect: 'slide',
-			spaceBetween: 50,
-		},
-		970: {
-			initialSlide: 1,
-			centeredSlides: true,
-			effect: 'coverflow',
-			slidesPerView: 3,
-			spaceBetween: 130,
-			coverflowEffect: {
-				rotate: 20,
-				stretch: 0,
-				depth: 100,
-				modifier: 1,
-				slideShadows: true,
-			},
-		},
-	},
+	direction: 'vertical',
+	mousewheel: true,
+	pagination: {
+		el: ".main__pagination",
+		clickable: true,
+	}
 });
 
+/*
 //СЛАЙДЕР QUOTES
 let quotes = new Swiper('.quotes__slider', {
 	speed: 800,
@@ -165,8 +130,8 @@ anim.addEventListener("click", function () {
 anim.addEventListener("transitionend", function () {
 	let arrow = document.querySelector(".quotes__img");
 	arrow.classList.remove('_active');
-})*/
-
+})
+*/
 
 
 /*
